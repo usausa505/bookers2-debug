a59afd3dae34cec74f0baba567fe2be9976a7f9f
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-02-01 08:01:29 +0000

    [Check point] bookers2-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,21 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
+      @user = current_user
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -41,6 +45,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
