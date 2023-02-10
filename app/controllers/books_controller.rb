a4c938723da1c0e09942752b0e8fa018f3245e25
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-02-10 13:22:41 +0000

    [Check point] bookers2-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
     @book = Book.new
-    @books = Book.all
     @user = current_user
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,7 @@ class Book < ApplicationRecord
 
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 
   def self.looks(search, word)
     if search == "perfect_match"
